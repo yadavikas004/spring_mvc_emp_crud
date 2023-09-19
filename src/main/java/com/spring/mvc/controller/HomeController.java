dfe808f8ddd8ba12ef5d822f2256b20f1d0dbeb6
Vikas Yadav <yadavikas088@gmail.com>
2023-09-19 08:36:23 +0530

    (development):everything working perfect changes some file

--- src/main/java/com/spring/mvc/controller/HomeController.java
+++ src/main/java/com/spring/mvc/controller/HomeController.java
@@ -4,21 +4,17 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.spring.mvc.dao.EmpDao;
 import com.spring.mvc.entity.Emp;
 
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LogManager.getLogger(HomeController.class);
-	
-	@Autowired
-	private EmpDao empDao;
+
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home() {
@@ -36,7 +32,6 @@ public class HomeController {
 	public String createEmp(@ModelAttribute Emp emp, HttpSession session) {
 //		System.out.println(emp);
 		logger.info("--------------Created Employee Details-------------");
-		int i = empDao.saveEmp(emp);
 		session.setAttribute("msg", "Register Successfully");
 		logger.info(emp);
 		logger.info("------Session Added Deatils----------"+session);
